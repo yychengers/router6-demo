341efc940c7e6850ba540b4d659ad9f1039ca4da
yuanyancheng <617217327@qq.com>
2023-02-28 07:47:58 +0800

    feat: first commit

---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './index.css';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div>hello world</div>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}></RouterProvider>
+  </React.StrictMode>
+);
